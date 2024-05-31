0c1c8c4ec3ba21c7461381f97a0f30f65871d65b
Xeon-s108 <xeon.s108@gmail.com>
2024-05-31 07:13:40 +0530

    Adding File

--- fbpseries.c
+++ fbpseries.c
@@ -1,2 +1,12 @@
+/*This program calculates the sum of n numbers in series : [n!/pow(n)] */
+
 #include<stdio.h>
 #include<math.h>
+void main ()
+{
+float i, n, sum;
+printf("Enter the number : ");
+scanf("%d",&n);
+
+
+}
