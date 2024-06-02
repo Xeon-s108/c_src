c4e7fb66d15b80ebf0753e4751b522a7b54d8e52
Xeon-s108 <xeon.s108@gmail.com>
2024-06-02 22:37:53 +0530

    fbp series creation and addition

---
+++ fbpseires.c
@@ -0,0 +1,13 @@
+/* This program prints the sum of [n!/npow(n)] series! */
+#include<stdio.h>
+#include<math.h>
+void main ()
+{
+float i , num, den, limit, temp;
+printf("Enter the number of last term in series : ");
+scanf("%f",&limit);
+for (i=1;i<=limit;i++)
+    {
+        
+    }
+}
