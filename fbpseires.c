a11fc6261ff887d4693eb05ff408bca5dac4ebd9
Xeon-s108 <xeon.s108@gmail.com>
2024-06-03 22:05:21 +0530

    update

--- fbpseires.c
+++ fbpseires.c
@@ -1,13 +1,22 @@
 /* This program prints the sum of [n!/npow(n)] series! */
-#include<stdio.h>
-#include<math.h>
-void main ()
+#include <stdio.h>
+#include <math.h>
+void main()
 {
-float i , num, den, limit, temp;
-printf("Enter the number of last term in series : ");
-scanf("%f",&limit);
-for (i=1;i<=limit;i++)
+    float i, num = 1, den = 1, lim, sum = 0;
+    printf("Enter the number of last term in series : ");
+    scanf("%f", &lim);
+    for (i = 1; i <= lim; i++)
     {
-        
+        num = num * num;
+        num++;
     }
+    {
+        den = pow(den, den);
+        den++;
+    }
+    {
+        sum = sum + (num / den);
+    }
+    printf("\nThe sum of [%f!/%fpow(%f)] is : %f", lim, lim, lim, sum);
 }
