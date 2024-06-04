bd9f5b8a32cf0867469e1c1df970c6895e3e430f
Xeon-s108 <xeon.s108@gmail.com>
2024-06-04 22:30:41 +0530

    bubble sort

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,7 +1,33 @@
-/*This program performs bubble sort and binary search!*/
+/*This program performs bubble sort and binary search.*/
 #include<stdio.h>
 void main ()
 {
-int i, j, k ;
+int i = 0, j, k , lim, temp;
 
+/*Taking range as input from user.*/
+printf("Enter the range : ");
+scanf("%d",&lim);
+int arr[lim];
+
+/*Taking array values from user.*/
+for (i = 0;i<lim;i++)
+    {
+        printf("\nEnter the value : ");
+        scanf("%d",&arr[i]);
+    }
+
+/*BUBBLE SORT HERE*/    
+for (i = 0; i<lim-1;i++);
+    {
+        for (j=0; j<lim-1-i;j++)
+            {
+                if (arr[j]>arr[j+1])
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+    }    
+
+printf("\nThe sorted array in ascending order is :");
+printf("\n%d ",arr);
 }
