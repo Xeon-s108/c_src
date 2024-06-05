7d5857e7d16a5b8db5ca2e568002e3450e3d0721
Xeon <xeons108@gmail.com>
2024-06-05 09:22:17 +0530

    Update bubble_sort.c

--- bubble_sort.c
+++ bubble_sort.c
@@ -22,9 +22,9 @@ void main()
         for (j = 0; j < lim - i; j++)
         {
             if (arr[j] > arr[j + 1])
-                temp = arr[j];
+                { temp = arr[j];
             arr[j] = arr[j + 1];
-            arr[j + 1] = temp;
+            arr[j + 1] = temp; }
         }
     }
 
